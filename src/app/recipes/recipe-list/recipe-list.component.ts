4776536a4bf4c01e1f42a30908ee82cf09ec31e2
Ludo Loops <ludoloops@prontonmail.com>
2020-08-11 02:15:22 +0200

    Feat: 85 Passing recipe Data with property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       'this is simply a test',
       'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.0PO3FoKphbvUDH24XRTGkwHaLH%26pid%3DApi&f=1'
     ),
+    new Recipe(
+      'Another  Recipe',
+      'this is not a test',
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.jISobxt-NW6-fsOTlp7GnAHaEK%26pid%3DApi&f=1'
+    ),
   ];
 
   constructor() {}

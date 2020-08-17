c0833d216a5eee7642289fa1327298261220b1cb
Ludo Loops <ludoloops@prontonmail.com>
2020-08-17 20:11:24 +0200

    Feat 283: Resolving Data Before Loading

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,32 +4,41 @@ import { Subject } from 'rxjs';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+// import { DataStorageService } from '../shared/data-storage.service';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fwiener-schnitzel-french-fries-breaded-steak-lemon-56757436.jpg&f=1&nofb=1',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'What else you need to say?',
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nrn.com%2Fsites%2Fnrn.com%2Ffiles%2Fburger_0.gif&f=1&nofb=1',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('buns', 2),
-        new Ingredient('tomato', 1),
-      ]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthumbs.dreamstime.com%2Fz%2Fwiener-schnitzel-french-fries-breaded-steak-lemon-56757436.jpg&f=1&nofb=1',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'What else you need to say?',
+  //     'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.nrn.com%2Fsites%2Fnrn.com%2Ffiles%2Fburger_0.gif&f=1&nofb=1',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('buns', 2),
+  //       new Ingredient('tomato', 1),
+  //     ]
+  //   ),
+  // ];
 
   constructor(private slService: ShoppingListService) {}
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
   getRecipes() {
+    // return this.dataStorageService.fetchRecipes();
     return this.recipes.slice();
   }
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
 const urlServer = 'https://ng-complete-guide-9f5a7.firebaseio.com/recipes.json';
@@ -14,5 +16,21 @@ export class DataStorageService {
     this.http.put(urlServer, recipes).subscribe((response) => {
       console.log(response);
     });
+  }
+
+  fetchRecipes() {
+    return this.http.get<Recipe[]>(urlServer).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 }

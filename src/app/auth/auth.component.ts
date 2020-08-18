f0d54f67a56162b502350259f17843bcc3430527
Ludo Loops <ludoloops@prontonmail.com>
2020-08-18 17:17:32 +0200

    Feat 297: Login Error Handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -24,23 +25,26 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.singup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          console.log(error);
-          this.error = 'An error occurred!';
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.singup(email, password);
     }
-    form.reset();
+
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMessage) => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+    // form.reset();
   }
 }
